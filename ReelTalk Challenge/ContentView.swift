bc0f922893d798f43ef5ed4343d383f9f11345b5
Kaleb Page <kp17501@ms.wasatch.edu>
2023-12-18 15:29:22 -0700

    Initial Commit

---
+++ ReelTalk Challenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReelTalk Challenge
+//
+//  Created by Kaleb Page on 12/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReelTalk Challenge/ReelTalk_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReelTalk_ChallengeApp.swift
+//  ReelTalk Challenge
+//
+//  Created by Kaleb Page on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ReelTalk_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
