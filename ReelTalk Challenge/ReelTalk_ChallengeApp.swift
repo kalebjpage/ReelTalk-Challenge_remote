7e123ce2961c0eb932612eea47fc3ae6e620e472
Kaleb Page <kp17501@ms.wasatch.edu>
2023-12-18 17:29:49 -0700

    Test

--- ReelTalk Challenge/ReelTalk_ChallengeApp.swift
+++ ReelTalk Challenge/ReelTalk_ChallengeApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ReelTalk_ChallengeApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SplashScreenView()
         }
     }
 }
--- ReelTalk Challenge/SplashScreenView.swift
+++ ReelTalk Challenge/SplashScreenView.swift
@@ -7,25 +7,50 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct SplashScreenView: View {
+    @State private var isActive = false
+    @State private var size = 0.8
+    @State private var opacity = 0.0
+    
     var body: some View {
         ZStack {
             Color("background")
                 .ignoresSafeArea()
-            VStack {
-                Image("Final Proposed Logo")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                Text("REEL TALK")
-                    .font(.title)
-                    .foregroundColor(Color.white)
+            if isActive == true {
+                FirstView()
+            } else {
+                VStack {
+                    VStack {
+                        Image("Final Proposed Logo")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                        Text("REEL TALK")
+                            .font(.custom("Avenir Next", size: 27))
+                            .foregroundColor(Color.white)
+                    }
+                    .scaleEffect(size)
+                    .opacity(opacity)
+                    .onAppear {
+                        withAnimation(.easeIn(duration: 1.2)) {
+                            self.size = 0.9
+                            self.opacity = 1.0
+                        }
+                    }
+                    .frame(width: 200, height: 200)
+                    .padding()
+                }
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                        withAnimation {
+                            self.isActive = true
+                        }
+                    }
+                }
             }
-            .frame(width: 200, height: 200)
-            .padding()
         }
     }
 }
 
 #Preview {
-    ContentView()
+    SplashScreenView()
 }
